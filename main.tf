013530698e1be1a16fd86a4364c59360441994d5
Mike Matera <matera@lifealgorithmic.com>
2025-11-20 01:58:56 +0000

    Add logging.

--- main.tf
+++ main.tf
@@ -22,6 +22,18 @@ resource "google_service_account" "vm_sa" {
   display_name = "VM Service Account"
 }
 
+resource "google_project_iam_member" "monitoring_writer" {
+  project = var.project
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.vm_sa.email}"
+}
+
+resource "google_project_iam_member" "logs_writer" {
+  project = var.project
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.vm_sa.email}"
+}
+
 resource "google_compute_disk" "persistent_disk" {
   name    = "vm-persistent-disk"
   type    = "pd-balanced"
@@ -74,6 +86,11 @@ resource "google_compute_instance" "web_instance" {
     network = google_compute_network.vpc_network.name
     access_config {
     }
+  }
+
+  service_account {
+    email  = google_service_account.vm_sa.email
+    scopes = ["cloud-platform"]
   }
 }
 
